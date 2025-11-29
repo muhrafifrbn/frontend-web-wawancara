f9b4b808041f51d33bdecd5326c5f8f563972b01
justrifky <rifkydoang2014@gmail.com>
2025-11-29 10:57:19 +0700

    test

--- src/page/JadwalTes.jsx
+++ src/page/JadwalTes.jsx
@@ -1,3 +1,7 @@
-const JadwalTes = () => {};
+import Dashboard from "../template/Dashboard";
+
+const JadwalTes = () => {
+  return <Dashboard title="Jadwal Tes"></Dashboard>;
+};
 
 export default JadwalTes;
